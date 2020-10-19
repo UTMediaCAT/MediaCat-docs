871da63e7edb42c6be3acdae96e115d5059d2f9f
Jacqueline <jacquelinechan2@hotmail.com>
2020-10-19 13:19:13 -0400

    ES lint for JavascriptDateTest.js

--- date-recognition/JavascriptDateTest.js
+++ date-recognition/JavascriptDateTest.js
@@ -1,199 +1,229 @@
-
-var JavascriptDate = require('./JavascriptDate');
-
-// const targetUrl = 'https://www.aljazeera.com/economy/2020/10/7/trump-urges-congress-to-pass-targeted-aid-after-halting-talks';
-
-const urlsPlain = [
-    'https://www.aljazeera.com/',
-    'https://www.aljazeera.com/economy/2020/10/7/trump-urges-congress-to-pass-targeted-aid-after-halting-talks',
-    'https://www.nytimes.com/',
-    'https://www.nytimes.com/2020/10/03/us/sean-conley-trump-doctor.html?action=click&module=Spotlight&pgtype=Homepage',
-    'https://www.cbc.ca/',
-    'https://www.cbc.ca/radio/thecurrent/the-current-for-oct-1-2020-1.5745953/made-in-america-how-trump-and-biden-s-rival-visions-for-economic-recovery-are-resonating-with-illinois-voters-1.5746657'
-    ]
+/*
+How to:
 
 
+Each url list corresponds to each test. We are testing the
+Javascript article-parser and the Javascript Metascrapper,
+so we have two seperate url lists. Each url list corresponds
+to a date list. In the date list, put the date you expect its corresponding
+article url to return from these libraries.
 
-const urls = [
-        'https://www.dw.com/uk/коментар-націоналізм-родом-зі-східної-європи/a-42081385',
-        'https://www.aljazeera.com/',
-        'https://www.aljazeera.com/economy/2020/10/7/trump-urges-congress-to-pass-targeted-aid-after-halting-talks',
-        'https://www.nytimes.com/',
-        'https://www.nytimes.com/2020/10/03/us/sean-conley-trump-doctor.html?action=click&module=Spotlight&pgtype=Homepage',
-        'https://www.cbc.ca/',
-        'https://www.cbc.ca/radio/thecurrent/the-current-for-oct-1-2020-1.5745953/made-in-america-how-trump-and-biden-s-rival-visions-for-economic-recovery-are-resonating-with-illinois-voters-1.5746657'
-        ]
+I put null on websites I expect the date parser will have difficulty finding
+a date for. Like home pages.
 
-const dates = [null,null, new Date(2020, 9, 7),null,new Date(2020, 9, 3), null, new Date(2020, 9, 2)]
+Here the Metascraper out performs the python libraries and the Javascript
+article-parser because it returns dates to urls that we expect to return
+null. We expected some urls to return null because of how difficult it
+usually is to extract them but the metascraper returns a guess anyways which is
+good news.
 
-const datesPlain = [null, new Date(2020, 9, 7),null,new Date(2020, 9, 3), null, new Date(2020, 9, 2)]
+Unlike the python library date guesser, it gives a date even on the home page.
+ This is fine behaviour.
 
-
-JavascriptDateTestFunc = function (url, date) {
-    // const url = 'https://goo.gl/MV8Tkh';
-    // const date = new Date(2017, 6, 17);
-    let promise = new Promise(function(resolve, reject){
-        JavascriptDate.articleParserfunction(url).then(dateReturned => {
-            
-                // console.log(dateReturned);
-                // console.log(dateReturned.published);
-                if (date == null || dateReturned == null || dateReturned.published == null || dateReturned.published == ""){
-
-                    if ((dateReturned == null) && (date == null)){
-                        console.log("MATCHED! " + url)
-                        // console.log(true)
-                        resolve (true)
-                    } else if ((dateReturned == null) && ( date == null)) {
-                        console.log("MATCHED! " + url)
-                        // console.log(true)
-                        resolve (true)
-                    } else if ( (dateReturned) && (dateReturned.published == null) && ( date == null)) {
-                        console.log("MATCHED! " + url)
-                        // console.log(true)
-                        resolve (true)
-                    } else if ( (dateReturned) && (dateReturned.published == "") && ( date == null)) {
-                        console.log("MATCHED! " + url)
-                        // console.log(true)
-                        resolve (true)
-                    } else {
-                        let string = ""
-                        string += "Not a match! " + url
-                        if (dateReturned) {
-                            string += " Date Returned: " + dateReturned.published;
-                        } else  {
-                            string += " Date Returned: " + dateReturned;
-
-                        }
-                        console.log(string + " Date Expected: " + date);
-                        resolve (false)
-                    }
-
-                } else {
-                    const date2 = new Date(dateReturned.published);
-                    date.setHours(0,0,0,0)
-                    date2.setHours(0,0,0,0)
-                    let string = " Date Returned: " + date2;
-                    string += " Date Expected: " + date;
-                    // console.log(date2 === date);
-                    let match = date2 === date
-                    string += " Match? : " + match
-                    console.log(string)
-                    resolve (date2 === date);
-                }
-            });
-            
-        });
-
-    return promise;
-};
+*/
 
 
-JavascriptDateTestPromise = function(){
-    let list = []
+import {articleParserfunction, metascraperfunction} from './JavascriptDate';
 
-    for (i = 0; i < urlsPlain.length; i++) {
-        list.push(JavascriptDateTestFunc(urlsPlain[i], datesPlain[i]))
+// const targetUrl =
+// 'https://www.aljazeera.com/economy/2020/10/7/trump-urges-congress-to-pass' +
+// '-targeted-aid-after-halting-talks';
 
-    };
+const urlsJavascriptDateTest = [
+  'https://www.aljazeera.com/',
+  'https://www.aljazeera.com/economy/2020/10/7/trump-urges-congress-to-' +
+  'pass-targeted-aid-after-halting-talks',
+  'https://www.nytimes.com/',
+  'https://www.nytimes.com/2020/10/03/us/sean-conley-trump-doctor.html?action=' +
+  'click&module=Spotlight&pgtype=Homepage',
+  'https://www.cbc.ca/',
+  'https://www.cbc.ca/radio/thecurrent/the-current-for-oct-1-2020' +
+   '-1.5745953/made-in-america-how-trump-and-biden-s-rival-visions-' +
+   'for-economic-recovery-are-resonating-with-illinois-voters-1.5746657',
+];
 
-    let promise = new Promise (function(resolve, reject) {
-        Promise.all(list).then((values) => {
-            console.log("VALUES")
-            console.log(values)
-            resolve(values)
-        })
-    })
-
-    return promise
-
-
-}
-
-
-JavascriptDateTest = function() {
-    JavascriptDateTestPromise().then((values) => {
-        let matches = 0
-        for (i=0; i < values.length; i++) {
-            if (values[i]) {
-                matches = matches + 1
-            }
-        }
+const datesJavascriptDateTest =
+  [null, new Date(2020, 9, 7), null,
+    new Date(2020, 9, 3), null, new Date(2020, 9, 2)];
 
 
-        console.log("There are " + matches + " matches out of " + values.length +" urls.")
+const urlsJavascriptMetascrapperTest = [
+  'https://www.dw.com/uk/коментар-націоналізм-родом-зі-східної-європи/a-42081385',
+  'https://www.aljazeera.com/',
+  'https://www.aljazeera.com/economy/2020/10/7/trump-urges-congress-to-pass-targeted' +
+  '-aid-after-halting-talks',
+  'https://www.nytimes.com/',
+  'https://www.nytimes.com/2020/10/03/us/sean-conley-trump-doctor.html?action=click&module' +
+  '=Spotlight&pgtype=Homepage',
+  'https://www.cbc.ca/',
+  'https://www.cbc.ca/radio/thecurrent/the-current-for-oct-1-2020-1.5745953' +
+  '/made-in-america-how-trump-and-biden-s-rival-visions-for-economic-recovery' +
+  '-are-resonating-with-illinois-voters-1.5746657',
+];
+
+const datesJavascriptMetascrapperTest =
+  [null, null, new Date(2020, 9, 7),
+    null, new Date(2020, 9, 3), null, new Date(2020, 9, 2)];
 
 
-
-    })
-}
-
-
-JavascriptMetascrapperTestFunc = function(urls){
-    let list = []
-    
-    for (i=0; i < urls.length; i++) {
-        let targetUrl = urls[i]
-        list.push(JavascriptDate.metascraperfunction(targetUrl))
-    };
-
-    let promise = new Promise (function(resolve, reject) {
-        Promise.all(list).then((values) => {
-            resolve(values)
-        })
-    })
-
-    return promise
-};
-
-JavascriptMetascrapperTest = function(){
-    JavascriptMetascrapperTestFunc(urls).then((values) => {
-        console.log(values)
-        let matches = 0
-
-        for (i=0; i < urls.length; i++) {
-
-            date1 = values[i].date
-            date2 = dates[i]
-
-            if (date1 === null || date2 === null) {
-                if ((date1 == null) && (date2==null)) {
-                    console.log("MATCHED! " + urls[i])
-                    console.log("returned: " + values[i].date)
-                    console.log("expected: " + dates[i])
-                    matches = matches + 1
-                } else {
-                    console.log("Not a match! " + urls[i])
-                    console.log("returned: " + values[i].date)
-                    console.log("expected: " + dates[i])
-                }
-            } else {
-
-                date1 = new Date(date1);
-
-                if (date1.setHours(0,0,0,0) == date2.setHours(0,0,0,0)){
-                    console.log("MATCHED! " + urls[i])
-                    console.log("returned: " + date1)
-                    console.log("expected: " + date2)
-                    matches = matches + 1
-                } else {
-                    console.log("Not a match! " + urls[i])
-                    console.log("returned: " + date1)
-                    console.log("expected: " + date2)
-                }
-
-            }
-
-        }
-
-        console.log("There are " + matches + " matches out of " + urls.length +" urls.")
-
+const javascriptDateTestFunc = function(url, date) {
+  // const url = 'https://goo.gl/MV8Tkh';
+  // const date = new Date(2017, 6, 17);
+  const promise = new Promise(function(resolve) {
+    articleParserfunction(url).then((dateReturned) => {
+    // console.log(dateReturned);
+    // console.log(dateReturned.published);
+      if (date == null || dateReturned == null ||
+        dateReturned.published == null ||
+        dateReturned.published == '') {
+        if ((dateReturned == null) && (date == null)) {
+          console.log('MATCHED! ' + url );
+          console.log('Date Returned: ' + dateReturned);
+          console.log('Date Expected: ' + date + '\n');
+          // console.log(true)
+          resolve(true);
+        } else if ( (dateReturned) &&
+         (dateReturned.published == null) &&
+         ( date == null)) {
+          console.log('MATCHED! ' + url );
+          console.log('Date Returned: ' + dateReturned.published);
+          console.log('Date Expected: ' + date + '\n');
+          // console.log(true)
+          resolve(true);
+        } else if ( (dateReturned) && (dateReturned.published == '') &&
+         ( date == null)) {
+          console.log('MATCHED! ' + url );
+          console.log('Date Returned: ' + dateReturned.published);
+          console.log('Date Expected: ' + date + '\n');
+          resolve(true);
+        } else {
+          console.log('Not a match! ' + url);
+          if (dateReturned) {
+            console.log(' Date Returned: ' + dateReturned.published);
+          } else {
+            console.log(' Date Returned: ' + dateReturned);
+          }
+          console.log(' Date Expected: ' + date + '\n');
+          resolve(false);
+        };
+      } else {
+        const date2 = new Date(dateReturned.published);
+        date.setHours(0, 0, 0, 0);
+        date2.setHours(0, 0, 0, 0);
+        // let string = " Date Returned: " + date2;
+        console.log(' Date Returned: ' + date2);
+        console.log(' Date Expected: ' + date);
+        // console.log(date2 === date);
+        const match = (date2 === date);
+        // string += " Match? : " + match
+        console.log(' Match? : ' + match + '\n');
+        // console.log(string + "\n")
+        resolve(date2 === date);
+      }
     });
-}
+  });
+  return promise;
+};
+
+
+const javascriptDateTestPromise = function() {
+  const list = [];
+
+  for (let i = 0; i < urlsJavascriptDateTest.length; i++) {
+    list.push(javascriptDateTestFunc(urlsJavascriptDateTest[i],
+        datesJavascriptDateTest[i]));
+  }
+
+  const promise = new Promise(function(resolve) {
+    Promise.all(list).then((values) => {
+      // console.log("VALUES")
+      // console.log(values) values of true and false
+      resolve(values);
+    });
+  });
+  return promise;
+};
+
+
+const javascriptDateTest = function() {
+  javascriptDateTestPromise().then((values) => {
+    let matches = 0;
+    for (let i=0; i < values.length; i++) {
+      if (values[i]) {
+        matches = matches + 1;
+      }
+    }
+    console.log('\x1b[33m%s\x1b[0m', 'There are ' +
+        matches + ' matches out of ' + values.length +' urls. \n');
+  });
+};
+
+
+const javascriptMetascrapperTestFunc = function(urls) {
+  const list = [];
+  for (let i=0; i < urls.length; i++) {
+    const targetUrl = urls[i];
+    list.push(metascraperfunction(targetUrl));
+  }
+
+  const promise = new Promise(function(resolve) {
+    Promise.all(list).then((values) => {
+      resolve(values);
+    });
+  });
+
+  return promise;
+};
+
+const javascriptMetascrapperTest = function() {
+  javascriptMetascrapperTestFunc(urlsJavascriptMetascrapperTest).then(
+      (values) => {
+        let matches = 0;
+
+        for (let i=0; i < urlsJavascriptMetascrapperTest.length; i++) {
+          let date1 = values[i].date;
+          const date2 = datesJavascriptMetascrapperTest[i];
+
+          if (date1 === null || date2 === null) {
+            if ((date1 == null) && (date2==null)) {
+              console.log('MATCHED! ' + urlsJavascriptMetascrapperTest[i]);
+              console.log('returned: ' + values[i].date);
+              console.log('expected: ' +
+               datesJavascriptMetascrapperTest[i] + '\n');
+              matches = matches + 1;
+            } else {
+              console.log('Not a match! ' + urlsJavascriptMetascrapperTest[i]);
+              console.log('returned: ' + values[i].date);
+              console.log('expected: ' +
+               datesJavascriptMetascrapperTest[i] + '\n');
+            }
+          } else {
+            date1 = new Date(date1);
+
+            if (date1.setHours(0, 0, 0, 0) == date2.setHours(0,0,0,0)) {
+              console.log('MATCHED! ' + urlsJavascriptMetascrapperTest[i]);
+              console.log('returned: ' + date1);
+              console.log('expected: ' + date2 + '\n');
+              matches = matches + 1;
+            } else {
+              console.log('Not a match! ' +
+                    urlsJavascriptMetascrapperTest[i]);
+              console.log('returned: ' + date1);
+              console.log('expected: ' + date2 + '\n');
+            }
+          }
+        }
+
+        console.log('\x1b[33m%s\x1b[0m', 'There are ' + matches +
+            ' matches out of ' + urlsJavascriptMetascrapperTest.length +
+            ' urls.' + '\n');
+      });
+};
 
 
 // Automative Testing
-JavascriptDateTest()
-JavascriptMetascrapperTest()
+javascriptDateTest();
+javascriptMetascrapperTest();
 
 // Manual Testing
 
